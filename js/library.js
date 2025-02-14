ebe64de8889ceb37e044d176dc6d2163ca53fc13
wenviva <quocanh010104@gmail.com>
2025-02-14 16:47:53 +0700

    PUSHED 2025-02-14 16:47:52

--- js/library.js
+++ js/library.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const bookList = document.getElementById("bookList");
     const searchInput = document.getElementById("searchInput");
     const searchButton = document.getElementById("searchButton");
+    const tagSearchInput = document.getElementById("tagSearchInput");
     const tagFilter = document.getElementById("tagFilter");
     const languageFilter = document.getElementById("languageFilter");
 
@@ -26,17 +27,16 @@ document.addEventListener("DOMContentLoaded", function () {
             const tagsDisplay = book.tags.join(", ");
 
             bookItem.innerHTML = `
-            <img src="${book.image}" alt="${book.title}">
-            <h3>${book.title}</h3>
-            <p><strong>Author:</strong> ${book.author}</p>
-            <p><strong>Language:</strong> ${book.language}</p>
-            <p><strong>Tags:</strong> ${tagsDisplay}</p>
-        `;
+                <img src="${book.image}" alt="${book.title}">
+                <h3>${book.title}</h3>
+                <p><strong>Author:</strong> ${book.author}</p>
+                <p><strong>Language:</strong> ${book.language}</p>
+                <p><strong>Tags:</strong> ${tagsDisplay}</p>
+            `;
 
             bookItem.addEventListener("click", () => {
                 window.location.href = `book.html?id=${book.id}`;
             });
-
 
             bookList.appendChild(bookItem);
         });
@@ -49,6 +49,15 @@ document.addEventListener("DOMContentLoaded", function () {
             (book) =>
                 book.title.toLowerCase().includes(query) ||
                 book.author.toLowerCase().includes(query)
+        );
+        displayBooks(filteredBooks);
+    }
+
+    // Search by tag function
+    function searchByTag() {
+        const tagQuery = tagSearchInput.value.toLowerCase();
+        const filteredBooks = books.filter((book) =>
+            book.tags.some((tag) => tag.toLowerCase().includes(tagQuery))
         );
         displayBooks(filteredBooks);
     }
@@ -76,9 +85,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // Event listeners
     searchButton.addEventListener("click", searchBooks);
     searchInput.addEventListener("input", searchBooks);
+    tagSearchInput.addEventListener("input", searchByTag);
     tagFilter.addEventListener("change", filterBooks);
     languageFilter.addEventListener("change", filterBooks);
 });
+
 // Load tags from tags.json
 fetch('./json/tags.json')
     .then(response => response.json())
